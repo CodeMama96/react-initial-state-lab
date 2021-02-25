b272509310ba5429e133821bac0860db118ab1de
Alicia Weenum <weenum.alicia@gmail.com>
2021-02-25 17:18:48 -0500

    commit tree ea5887475e084e0a541478b604b05aa0535f4371

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,28 @@
 // your Bomb code here!
+import React, { Component } from 'react';
+import ImageSlider from './ImageSlider'
+
+
+export default class Bomb extends Component {
+    constructor(props) {
+        super()
+        this.state = {
+          secondsLeft: props.initialCount
+       
+        }
+      }
+    render(){
+        if (this.state.secondsLeft === 0){
+        return (
+            <div>Boom!</div>
+        )
+        }else{
+            return (
+            <div>
+            {this.props.initialCount} seconds left before I go boom!
+            </div>
+            )
+        }
+    }
+
+}
